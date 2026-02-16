547085d2c972b111e2a5fff64fbf669085438462
ibitabadger <126259117+ibitabadger@users.noreply.github.com>
2026-02-15 21:37:04 -0500

    Swagger fixes

--- lib/swagger.ts
+++ lib/swagger.ts
@@ -9,7 +9,7 @@ const options: swaggerJSDoc.Options = {
       description: 'Documentación de las APIs',
     },
   },
-  apis: ['./pages/api/**/*.ts', "app/api/**/*.ts"], // Rutas archivos de API en local y producción
+  apis: ['./pages/api/**/*.ts', './pages/api/**/*.js', "app/api/**/*.ts", "app/api/**/*.js"], // Rutas archivos de API en local y producción
 };
 
 export const spec = swaggerJSDoc(options);
